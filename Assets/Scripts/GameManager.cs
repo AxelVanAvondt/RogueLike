37758bd23635397677b18bad9982848c43f8342c
AxelVanAvondt <axel.vanavondt@student.arcadiascholen.be>
2024-05-16 11:16:09 +0200

    Enemies Added
    I WANNA MAKE MY OWN TITLE

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,11 +17,22 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    public List<Actor> Enemies = new List<Actor>();
 
     public static GameManager Get { get => instance; }
 
     public Actor GetActorAtLocation(Vector3 location)
     {
         return null;
+    }
+    public GameObject CreateActor(string name, Vector2 position)
+    {
+        GameObject actor = Instantiate(Resources.Load<GameObject>($"Prefabs/{name}"), new Vector3(position.x + 0.5f, position.y + 0.5f, 0), Quaternion.identity);
+        actor.name = name;
+        return actor;
+    }
+    public void AddEnemy(Actor enemy)
+    {
+        Enemies.Add(enemy);
     }
 }
--- Assets/Scripts/Map/DungeonGenerator.cs
+++ Assets/Scripts/Map/DungeonGenerator.cs
@@ -7,6 +7,7 @@ public class DungeonGenerator : MonoBehaviour
     private int width, height;
     private int maxRoomSize, minRoomSize;
     private int maxRooms;
+    private int maxEnemies;
     List<Room> rooms = new List<Room>();
 
     public void SetSize(int width, int height)
@@ -24,6 +25,10 @@ public class DungeonGenerator : MonoBehaviour
     public void SetMaxRooms(int max)
     {
         maxRooms = max;
+    }
+    public void SetMaxEnemies(int max)
+    {
+        maxEnemies = max;
     }
 
     public void Generate()
@@ -74,10 +79,10 @@ public class DungeonGenerator : MonoBehaviour
             {
                 TunnelBetween(rooms[rooms.Count - 1], room);
             }
-
+            PlaceEnemies(room, maxEnemies);
             rooms.Add(room);
         }
-        var player = MapManager.Get.CreateActor("Player", rooms[0].Center());
+        var player = GameManager.Get.CreateActor("Player", rooms[0].Center());
     }
 
     private bool TrySetWallTile(Vector3Int pos)
@@ -145,4 +150,29 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
     }
+
+    private void PlaceEnemies(Room room, int maxEnemies)
+    {
+        // the number of enemies we want 
+        int num = Random.Range(0, maxEnemies + 1);
+
+        for (int counter = 0; counter < num; counter++)
+        {
+            // The borders of the room are walls, so add and substract by 1 
+            int x = Random.Range(room.X + 1, room.X + room.Width - 1);
+            int y = Random.Range(room.Y + 1, room.Y + room.Height - 1);
+
+            // create different enemies 
+            if (Random.value < 0.5f)
+            {
+                GameManager.Get.CreateActor("HotDemon", new Vector2(x, y));
+            }
+            else
+            {
+                GameManager.Get.CreateActor("DragonBF", new Vector2(x, y));
+            }
+        }
+    }
+
 }
+//^(?([^\r\n])\s)*\r?$\r?\n
