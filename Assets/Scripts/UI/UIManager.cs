c7e4f0ccc75b055b4382ca3218d0808230960abc
AxelVanAvondt <axel.vanavondt@student.arcadiascholen.be>
2024-05-25 11:42:21 +0200

    Healthbar and Messages
    HEHEHE

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public static UIManager Get { get => instance; }
+
+    [Header("Documents")]
+    public HealthBar healthBar;
+    public Messages messages;
+
+
+    public void UpdateHealth(int current, int max)
+    {
+        healthBar.GetComponent<HealthBar>().SetValues(current, max);
+    }
+
+    public void AddMessage(string message, Color color)
+    {
+        messages.GetComponent<Messages>().AddMessage(message, color);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
